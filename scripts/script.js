2f2c7619aa5770be8a88faf540f0661f348d5f15
ermiralliu <mirialliu@gmail.com>
2024-06-06 16:17:06 +0200

    fixed the body overflow and made modal windows for contact and about us

--- scripts/script.js
+++ scripts/script.js
@@ -23,27 +23,19 @@ async function get(str, link){                   //eeee se do i rregulloj, dhe d
                     //console.log(JSON.stringify(array));
     let view ="";
     array.forEach(element=>{  
-        view += `<figure class="body-div" onclick="makeModal(${element.id})"><img class="body-img" alt="a ${currentAnimal}" src="${element.image}"/><figcaption>`;
-        //let temp = element.image;         //kjo ishte qe mos te shkruaja imazhin se kartat fillestare i kishin te gjitha te dhenat
-        //delete element.image;
-        // for([key, value] of Object.entries(element))
-        //     view+=`<tr><td>${key}</td><td>${value}</td></tr>`;
-        view+=`Name:  ${element.name}<br>`
-            +`Origin:  ${element.origin || element.place_of_found}`     //se origjina esht bosh per zogjt psh
-            +"</figcaption></figure>";
-        //element.image = temp;
-    });
+        view += `<figure class="body-div" >
+                <img onclick="makeModal(${element.id})" class="body-img" alt="a ${currentAnimal}" src="${element.image}"/>
+                <figcaption>
+                Name:  ${element.name}<br>
+                Origin:  ${element.origin || element.place_of_found}     
+                </figcaption></figure>`;
+    });                                     // Origin me OR, se origjina esht bosh per zogjt psh
     currentArray = array;
     sect.innerHTML = view;
     if(str === "dogs" || str === "cats" || str === "birds")
         currentAnimal = str;                                            //to keep track of the current animal
     lastSearched = str;
     //from here on it doesn't work, maybe cause the document hasn't processed it fully yet
-    let bodyDivArray = document.querySelectorAll("body-div");
-    console.log(bodyDivArray);
-    for(let i=0; i< bodyDivArray.length; ++i){
-        bodyDivArray[i].addEventListener("click", ()=>makeModal(array[i]));
-    }
 }
 
 
@@ -55,12 +47,16 @@ function makeModal(id){
     for([key,value] of Object.entries(object))
         html_string += `<tr><td>${key}</td><td>${value}</td></tr>`;
     html_string += `</table>`;
+    modalWindow(html_string);
+}
+function modalWindow(str){
     let moDiv = document.getElementById("mod");                 //marrim div me kte id dhe i japim klasen e cila ne css i jep vetite e duhura
     moDiv.setAttribute("onclick", "unMakeModal()")
     //moDiv.onclick = "";
     moDiv.className = "modal";
-    moDiv.innerHTML = html_string;
+    moDiv.innerHTML = str;
 }
+
 function unMakeModal(){
     let moDiv = document.getElementById("mod");
     moDiv.innerHTML = "";
@@ -84,8 +80,7 @@ searchBar.addEventListener("keypress", (event)=>{
     get(searchBar.value, `https://freetestapi.com/api/v1/${currentAnimal}?search=${searchBar.value}`);
 } );
 
-const searchButton = document.getElementById("search-button");
-searchButton.addEventListener("click", ()=> get(searchBar.value, `https://freetestapi.com/api/v1/${currentAnimal}?search=${searchBar.value}`));
+document.getElementById("search-button").addEventListener("click", ()=> get(searchBar.value, `https://freetestapi.com/api/v1/${currentAnimal}?search=${searchBar.value}`));
 
 document.getElementById("filter").addEventListener("click", ()=>{
     if( isNaN(searchBar.value))
@@ -93,3 +88,17 @@ document.getElementById("filter").addEventListener("click", ()=>{
     get(searchBar.value, `https://freetestapi.com/api/v1/${currentAnimal}?limit=${searchBar.value}`);
 });
 document.getElementById("sort").addEventListener("click", ()=> get("sortaWeird" ,`https://freetestapi.com/api/v1/${currentAnimal}?sort=name&order=dec`));
+
+document.getElementById("about").addEventListener("click", ()=> modalWindow(`<div><h2>About Us</h2><p> Welcome to <b>Example</b> 
+    where we share information related to Animal Gallery. 
+    We're dedicated to providing you the very best information and knowledge of the above mentioned topics.</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>
+`));
+document.getElementById("contact").addEventListener("click", ()=> modalWindow(`<div><h2>Contacts</h2><p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
+    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+    non   proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p> Email: mirialliu@gmail.com <br>
+        Phone: +355 6x xx xx xxx
+    </p>
+    </div>
+`));
